cb7a8a142e8c7f09b13a303866f6f369956aae11
snicholasbarton <12804431+snicholasbarton@users.noreply.github.com>
2026-01-13 14:42:41 -0500

    tweaks

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,10 +1,22 @@
-import { motion } from 'framer-motion';
-import { Briefcase, GraduationCap, Code, Megaphone, UserRound, Quote } from 'lucide-react';
-import { Section } from '../components/ui/Section';
-import { SubHeading, Paragraph, UnorderedList, ListItem, Link} from '../components/ui/Typography';
-import styles from './Resume.module.css';
-import { Tooltip } from '../components/ui/Tooltip';
-
+import { motion } from "framer-motion";
+import {
+  Briefcase,
+  GraduationCap,
+  Code,
+  Megaphone,
+  UserRound,
+  Quote,
+} from "lucide-react";
+import { Section } from "../components/ui/Section";
+import {
+  SubHeading,
+  Paragraph,
+  UnorderedList,
+  ListItem,
+  Link,
+} from "../components/ui/Typography";
+import styles from "./Resume.module.css";
+import { Tooltip } from "../components/ui/Tooltip";
 
 interface TimelineSubItem {
   subtitle: string;
@@ -21,31 +33,32 @@ interface TimelineItemProps {
 const TimelineItem = ({ title, icon, subItems }: TimelineItemProps) => {
   return (
     <div>
-      <div className={styles.timelineIconContainer}>
-        {icon}
-      </div>
+      <div className={styles.timelineIconContainer}>{icon}</div>
 
-      <SubHeading className={`${styles.timelineTitle} mb-4`}>{title}</SubHeading>
+      <SubHeading className={`${styles.timelineTitle} mb-4`}>
+        {title}
+      </SubHeading>
 
       <div className="mt-3">
         {subItems.map((item, index) => (
-        <motion.div
+          <motion.div
             initial={{ opacity: 0, x: -50 }}
             whileInView={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.5 }}
             viewport={{ once: true }}
             className={styles.timelineItem}
-            >
-          <div key={index}>
-            <div className={styles.timelineHeader}>
-              <div className={styles.timelineSubtitle}>{item.subtitle}</div>
-              <span className={styles.timelineDate}>{item.date}</span>
+          >
+            <div key={index}>
+              <div className={styles.timelineHeader}>
+                <div className={styles.timelineSubtitle}>{item.subtitle}</div>
+                <span className={styles.timelineDate}>{item.date}</span>
+              </div>
+              <Paragraph className={styles.timelineDescription}>
+                {item.description}
+              </Paragraph>
             </div>
-            <Paragraph className={styles.timelineDescription}>
-              {item.description}
-            </Paragraph>
-          </div>
-        </motion.div>))}
+          </motion.div>
+        ))}
       </div>
     </div>
   );
@@ -85,76 +98,163 @@ const ReferenceQuote = ({ text, author, role }: ReferenceQuoteProps) => (
   </motion.div>
 );
 
-const sseDescription = (<UnorderedList>
-    <ListItem><b>Technical Vision:</b> Defined long-term product strategy to grow streaming platform usage 100x from 2023-2025</ListItem>
-    <ListItem><b>High Availability Infrastructure:</b> Architected a no-downtime orchestration framework for stateful Flink jobs, enabling infrastructure changes, framework updates, and logic deployments without data loss or interruption</ListItem>
-    <ListItem><b>Systems Performance:</b> Spearheaded a performance overhaul by tuning configurations, addressing algorithmic bottlenecks and improving API, database, and serialisation schemas. Increased system job capacity by 20x, reduced DB transaction volume by 40%, and increased max pipeline throughput by 50x</ListItem>
-    <ListItem><b>Cross-functional Leadership:</b> Led stability work with Kafka infra team reducing outages caused by manual migrations and optimising client libraries, reducing CPU utilisation by 20x; collaborated with observability team to build user-defined monitors, in-platform metrics and automated health monitoring
-</ListItem>
-    <ListItem><b>Engineering Excellence:</b> Led architecture reviews; conducted technical interviews for industry hires; acted as the L3 escalation point for global streaming outages, resolving 1000+ issues; developed engineering talent
-</ListItem>
-  </UnorderedList>)
-const tlDescription = (<UnorderedList>
-    <ListItem><b>Strategic Technical Leadership:</b> Managed technical engagement with customers as chief technical contact; led engineering effort for joint venture with oil super-major; defined product roadmaps and technical strategy</ListItem>
-    <ListItem><b>Mentorship:</b> Mentored new engineers, developed growth frameworks, designed and executed detailed performance reviews; led design and architecture reviews</ListItem>
-  </UnorderedList>)
-const fdeDescription = (<UnorderedList>
-    <ListItem><b>Full-Stack Engineering:</b> Developed an end-to-end application for real-time stream processing in service of a $10M+ contract; built multi-tenant orchestration engine managing simulation systems in customer networks</ListItem>
-    <ListItem><b>Data Pipelining:</b> Architected and built data pipelines for structured & unstructured data with Spark and Flink</ListItem>
-  </UnorderedList>)
-const oxfordDescription = (<UnorderedList>
-    <ListItem>Thesis: A Meshless Galerkin Method for Dirichlet Problems using Radial Basis Functions</ListItem>
-</UnorderedList>)
-const firedrakeTooltip = <span><Link href={"https://firedrakeproject.org/"}>Firedrake</Link> is an automated system for the solution of partial differential equations using the finite element method (FEM). Firedrake uses sophisticated code generation to provide mathematicians, scientists, and engineers with a very high productivity way to create sophisticated high performance simulations.</span>
-const imperialDescription = (<UnorderedList>
-    <ListItem>Thesis: An Automatic Finite Element Solver for Complex-Valued PDEs</ListItem>
-    <ListItem>Developed peer-reviewed Python code extending <Tooltip content={firedrakeTooltip}>Firedrake</Tooltip>'s symbolic algebra to the complex numbers.</ListItem>
-</UnorderedList>)
-const awardTooltip = "Awarded annually to that member of the Princeton men's varsity squash team who, through enthusiasm, ability, sportsmanship and leadership has contributed most to the sport and his team."
-const princetonDescription = (<UnorderedList>
-    <ListItem>Thesis: A Bayesian Method to Determine the Distribution of Transiting Exoplanets in Binary, Multiplanet Kepler Systems</ListItem>
+const sseDescription = (
+  <UnorderedList>
+    <ListItem>
+      <b>Technical Vision:</b> Defined long-term product strategy to grow
+      streaming platform usage 100x from 2023-2025
+    </ListItem>
+    <ListItem>
+      <b>High Availability Infrastructure:</b> Architected a no-downtime
+      orchestration framework for stateful Flink jobs, enabling infrastructure
+      changes, framework updates, and logic deployments without data loss or
+      interruption
+    </ListItem>
+    <ListItem>
+      <b>Systems Performance:</b> Spearheaded a performance overhaul by tuning
+      configurations, addressing algorithmic bottlenecks and improving API,
+      database, and serialisation schemas. Increased system job capacity by 20x,
+      reduced DB transaction volume by 40%, and increased max pipeline
+      throughput by 50x
+    </ListItem>
+    <ListItem>
+      <b>Cross-functional Leadership:</b> Led stability work with Kafka infra
+      team reducing outages caused by manual migrations and optimising client
+      libraries, reducing CPU utilisation by 20x; collaborated with
+      observability team to build user-defined monitors, in-platform metrics and
+      automated health monitoring
+    </ListItem>
+    <ListItem>
+      <b>Engineering Excellence:</b> Led architecture reviews; conducted
+      technical interviews for industry hires; acted as the L3 escalation point
+      for global streaming outages, resolving 1000+ issues; developed
+      engineering talent
+    </ListItem>
+  </UnorderedList>
+);
+const tlDescription = (
+  <UnorderedList>
+    <ListItem>
+      <b>Strategic Technical Leadership:</b> Managed technical engagement with
+      customers as chief technical contact; led engineering effort for joint
+      venture with oil super-major; defined product roadmaps and technical
+      strategy
+    </ListItem>
+    <ListItem>
+      <b>Mentorship:</b> Mentored new engineers, developed growth frameworks,
+      designed and executed detailed performance reviews; led design and
+      architecture reviews
+    </ListItem>
+  </UnorderedList>
+);
+const fdeDescription = (
+  <UnorderedList>
+    <ListItem>
+      <b>Full-Stack Engineering:</b> Developed an end-to-end application for
+      real-time stream processing in service of a $10M+ contract; built
+      multi-tenant orchestration engine managing simulation systems in customer
+      networks
+    </ListItem>
+    <ListItem>
+      <b>Data Pipelining:</b> Architected and built data pipelines for
+      structured & unstructured data with Spark and Flink
+    </ListItem>
+  </UnorderedList>
+);
+const oxfordDescription = (
+  <UnorderedList>
+    <ListItem>
+      Thesis: A Meshless Galerkin Method for Dirichlet Problems using Radial
+      Basis Functions
+    </ListItem>
+  </UnorderedList>
+);
+const firedrakeTooltip = (
+  <span>
+    <Link href={"https://firedrakeproject.org/"}>Firedrake</Link> is an
+    automated system for the solution of partial differential equations using
+    the finite element method (FEM). Firedrake uses sophisticated code
+    generation to provide mathematicians, scientists, and engineers with a very
+    high productivity way to create sophisticated high performance simulations.
+  </span>
+);
+const imperialDescription = (
+  <UnorderedList>
+    <ListItem>
+      Thesis: An Automatic Finite Element Solver for Complex-Valued PDEs
+    </ListItem>
+    <ListItem>
+      Developed peer-reviewed Python code extending{" "}
+      <Tooltip content={firedrakeTooltip}>Firedrake</Tooltip>'s symbolic algebra
+      to the complex numbers.
+    </ListItem>
+  </UnorderedList>
+);
+const awardTooltip =
+  "Awarded annually to that member of the Princeton men's varsity squash team who, through enthusiasm, ability, sportsmanship and leadership has contributed most to the sport and his team.";
+const princetonDescription = (
+  <UnorderedList>
+    <ListItem>
+      Thesis: A Bayesian Method to Determine the Distribution of Transiting
+      Exoplanets in Binary, Multiplanet Kepler Systems
+    </ListItem>
     <ListItem>Elected to Sigma Xi Research Society</ListItem>
-    <ListItem>Varsity Squash: <Tooltip content={awardTooltip}>2016 George McFarland Award</Tooltip>, 2013 Most Improved Player, 2014 Sloane Sportsmanship Award. 2014 & 2015 Ivy League Champions.</ListItem>
-</UnorderedList>)
+    <ListItem>
+      Varsity Squash:{" "}
+      <Tooltip content={awardTooltip}>2016 George McFarland Award</Tooltip>,
+      2013 Most Improved Player, 2014 Sloane Sportsmanship Award. 2014 & 2015
+      Ivy League Champions.
+    </ListItem>
+  </UnorderedList>
+);
 
 export const Resume = () => {
-    return (
-        <Section>
+  return (
+    <Section>
       <div id="resume" className={styles.resumeSection}>
         <div className={styles.sectionBlock}>
-            <SubHeading className={styles.experienceHeading}>
-                <UserRound className={styles.experienceIcon} /> Summary
-            </SubHeading>
-            <div className="space-y-2 -mb-8">
-                <Paragraph>
-                    Senior software engineer with 7+ years experience architecting and scaling mission-critical distributed systems. Proven track record at Palantir leading the growth of Foundry’s streaming platform, scaling usage 100x and building stateful, zero-downtime upgrade orchestration for 1000+ Flink clusters. <Link href="http://www.rhodes.bm/rhelects.htm#BARTON,_S._Nicholas">Rhodes Scholar</Link> with graduate degrees in mathematics.
-                </Paragraph>
-            </div>
+          <SubHeading className={styles.experienceHeading}>
+            <UserRound className={styles.experienceIcon} /> Summary
+          </SubHeading>
+          <div className="space-y-2 -mb-8">
+            <Paragraph>
+              Senior software engineer with 7+ years experience architecting and
+              scaling mission-critical distributed systems. Proven track record
+              at Palantir leading the growth of Foundry’s streaming platform,
+              scaling usage 100x and building stateful, zero-downtime upgrade
+              orchestration for 1000+ Flink clusters.{" "}
+              <Link href="http://www.rhodes.bm/rhelects.htm#BARTON,_S._Nicholas">
+                Rhodes Scholar
+              </Link>{" "}
+              with graduate degrees in mathematics.
+            </Paragraph>
+          </div>
         </div>
-         <div className={styles.sectionBlock}>
-            <SubHeading className={styles.experienceHeading}>
-                <Megaphone className={styles.experienceIcon} /> Professional References
-            </SubHeading>
-            <div className={styles.referencesGrid}>
-                <ReferenceQuote
-                    text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                    author="Jane Doe"
-                    role="CTO, TechCorp"
-                />
-                <ReferenceQuote
-                    text="Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-                    author="John Smith"
-                    role="VP of Engineering, DataSystems"
-                />
-                <ReferenceQuote
-                    text="Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-                    author="Emily White"
-                    role="Senior Product Manager, InnovateInc"
-                />
-            </div>
-            <div className={styles.referencesNotice}>
-                Full references available upon request
-            </div>
+        <div className={styles.sectionBlock}>
+          <SubHeading className={styles.experienceHeading}>
+            <Megaphone className={styles.experienceIcon} /> Professional
+            References
+          </SubHeading>
+          <div className={styles.referencesGrid}>
+            <ReferenceQuote
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+              author="Jane Doe"
+              role="CTO, TechCorp"
+            />
+            <ReferenceQuote
+              text="Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+              author="John Smith"
+              role="VP of Engineering, DataSystems"
+            />
+            <ReferenceQuote
+              text="Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+              author="Emily White"
+              role="Senior Product Manager, InnovateInc"
+            />
+          </div>
+          <div className={styles.referencesNotice}>
+            Full references available upon request
+          </div>
         </div>
         <div className={styles.sectionBlock}>
           <SubHeading className={styles.experienceHeading}>
@@ -168,18 +268,19 @@ export const Resume = () => {
                 {
                   subtitle: "Senior Software Engineer",
                   date: "2022 - 2025",
-                  description: sseDescription
+                  description: sseDescription,
                 },
                 {
-                  subtitle: "Technical Lead, Forward Deployed Software Engineer",
+                  subtitle:
+                    "Technical Lead, Forward Deployed Software Engineer",
                   date: "2019 - 2022",
-                  description: tlDescription
+                  description: tlDescription,
                 },
                 {
                   subtitle: "Forward Deployed Software Engineer",
                   date: "2018 - 2019",
-                  description: fdeDescription
-                }
+                  description: fdeDescription,
+                },
               ]}
             />
           </div>
@@ -195,10 +296,11 @@ export const Resume = () => {
               icon={<GraduationCap size={16} />}
               subItems={[
                 {
-                  subtitle: "MSc with Merit, Mathematical Modelling and Scientific Computing",
+                  subtitle:
+                    "MSc with Merit, Mathematical Modelling and Scientific Computing",
                   date: "2018",
-                  description: oxfordDescription
-                }
+                  description: oxfordDescription,
+                },
               ]}
             />
             <TimelineItem
@@ -208,8 +310,8 @@ export const Resume = () => {
                 {
                   subtitle: "MSc with Distinction, Applied Mathematics",
                   date: "2017",
-                  description: imperialDescription
-                }
+                  description: imperialDescription,
+                },
               ]}
             />
             <TimelineItem
@@ -219,8 +321,8 @@ export const Resume = () => {
                 {
                   subtitle: "AB with Honours, Astrophysical Sciences",
                   date: "2016",
-                  description: princetonDescription
-                }
+                  description: princetonDescription,
+                },
               ]}
             />
           </div>
@@ -231,11 +333,23 @@ export const Resume = () => {
             <Code className={styles.experienceIcon} /> Skills
           </SubHeading>
           <div className={styles.skillsContainer}>
-            {["React", "TypeScript", "Node.js", "Python", "AWS", "Docker", "GraphQL", "Tailwind CSS", "PostgreSQL", "Redis"].map(skill => (
+            {[
+              "React",
+              "TypeScript",
+              "Node.js",
+              "Python",
+              "AWS",
+              "Docker",
+              "GraphQL",
+              "Tailwind CSS",
+              "PostgreSQL",
+              "Redis",
+            ].map((skill) => (
               <SkillPill key={skill} skill={skill} />
             ))}
           </div>
         </div>
       </div>
-      </Section>)
-}
+    </Section>
+  );
+};
